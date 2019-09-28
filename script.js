b4a7a5d7c22132e5f456481b70c05cf383a28e8c
Steve Farrar <sfarrar@sfarrarmac.local>
2019-09-28 17:36:52 -0400

    Javascript slider

---
+++ script.js
@@ -0,0 +1,27 @@
+var shade= {
+  img:"design-assets/product-red.jpg"
+  subtitle: "Rock-N-Stroll"
+  color: "Red"
+  Price: $199
+}
+
+var shade= {
+  img:"design-assets/product-green.jpg"
+  subtitle: "Rock-N-Stroll"
+  color: "Red"
+  Price: $199
+}
+
+var shade= {
+  img:"design-assets/product-blue.jpg"
+  subtitle: "Rock-N-Stroll"
+  color: "Red"
+  Price: $199
+}
+
+var shade= {
+  img:"design-assets/product-black.jpg"
+  subtitle: "Rock-N-Stroll"
+  color: "Red"
+  Price: $199
+}
